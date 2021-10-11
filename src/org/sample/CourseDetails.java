ad1c418ba2084b017eb3b5367dd6ff2d08c9d168
Sabasiva06 <sabsiva06@gmail.com>
2021-10-11 14:14:19 +0530

    Just project push

---
+++ src/org/sample/CourseDetails.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseDetails {
+
+	public static void main(String[] args) {
+
+		int i=10_000;
+		System.out.println(i);
+		
+	}
+
+}
---
+++ src/org/sample/Sample.java
@@ -0,0 +1,19 @@
+package org.sample;
+
+public class Sample {
+
+	static int a;//-->static variable
+	int b;// instance variable
+
+	public void add() {
+		a = 10;
+		int a = 10;//local variable
+		System.out.println(a);
+	}
+
+	
+	public static void main(String[] args) {
+
+	
+	}
+}

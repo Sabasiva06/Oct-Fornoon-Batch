aa69df82e4abf6d1d05ea46555ac8db8b370a749
Sabasiva06 <sabsiva06@gmail.com>
2021-10-13 13:25:01 +0530

    B works

--- src/org/sample/CourseDetails.java
+++ src/org/sample/CourseDetails.java
@@ -7,9 +7,13 @@ public class CourseDetails {
 
 	public static void main(String[] args) {
 
-		int i=10_000;
+		int i = 10_000;
 		System.out.println(i);
-		
+		System.out.println("Selenium");
+		System.out.println("Api Testing");
+		System.out.println("Appium");
+		System.out.println("Rest Assured");
+		System.out.println("Python");
 	}
 
 }
